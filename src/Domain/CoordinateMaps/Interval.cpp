090df8bb91bd4fd92e7e4f182ed5310a40003953
Nils Vu <owls@nilsvu.de>
2024-11-13 13:02:09 -0800

    Use autodiff to test map Jacobians

--- src/Domain/CoordinateMaps/Interval.cpp
+++ src/Domain/CoordinateMaps/Interval.cpp
@@ -253,18 +253,34 @@ bool operator==(const CoordinateMaps::Interval& lhs,
 // Explicit instantiations
 #define DTYPE(data) BOOST_PP_TUPLE_ELEM(0, data)
 
-#define INSTANTIATE(_, data)                                                   \
-  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 1>                 \
-  Interval::operator()(const std::array<DTYPE(data), 1>& source_coords) const; \
-  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 1, Frame::NoFrame>   \
-  Interval::jacobian(const std::array<DTYPE(data), 1>& source_coords) const;   \
-  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 1, Frame::NoFrame>   \
-  Interval::inv_jacobian(const std::array<DTYPE(data), 1>& source_coords)      \
+#define INSTANTIATE(_, data)                                   \
+  template std::array<tt::remove_cvref_wrap_t<DTYPE(data)>, 1> \
+  Interval::operator()(const std::array<DTYPE(data), 1>& source_coords) const;
+
+#define INSTANTIATE_JAC(_, data)                                             \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 1, Frame::NoFrame> \
+  Interval::jacobian(const std::array<DTYPE(data), 1>& source_coords) const; \
+  template tnsr::Ij<tt::remove_cvref_wrap_t<DTYPE(data)>, 1, Frame::NoFrame> \
+  Interval::inv_jacobian(const std::array<DTYPE(data), 1>& source_coords)    \
       const;
 
 GENERATE_INSTANTIATIONS(INSTANTIATE, (double, DataVector,
                                       std::reference_wrapper<const double>,
                                       std::reference_wrapper<const DataVector>))
+GENERATE_INSTANTIATIONS(INSTANTIATE_JAC,
+                        (double, DataVector,
+                         std::reference_wrapper<const double>,
+                         std::reference_wrapper<const DataVector>))
+
+#ifdef SPECTRE_AUTODIFF
+#include <autodiff/forward/dual.hpp>
+#include <autodiff/forward/real.hpp>
+#include <autodiff/reverse/var.hpp>
+GENERATE_INSTANTIATIONS(INSTANTIATE,
+                        (autodiff::dual, autodiff::real, autodiff::var))
+#endif  // SPECTRE_AUTODIFF
+
 #undef DTYPE
 #undef INSTANTIATE
+#undef INSTANTIATE_JAC
 }  // namespace domain::CoordinateMaps
