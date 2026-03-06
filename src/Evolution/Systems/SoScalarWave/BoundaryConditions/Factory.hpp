ca95aa9d1cca3276b9cd0e89d6131af39b2c39b2
hen-w <henry.huang@black-holes.org>
2026-03-05 22:41:45 -0500

    Add time derivative dirichlet bc to LDG SoScalarWave

--- src/Evolution/Systems/SoScalarWave/BoundaryConditions/Factory.hpp
+++ src/Evolution/Systems/SoScalarWave/BoundaryConditions/Factory.hpp
@@ -8,12 +8,13 @@
 #include "Domain/BoundaryConditions/Periodic.hpp"
 #include "Evolution/Systems/SoScalarWave/BoundaryConditions/BoundaryCondition.hpp"
 #include "Evolution/Systems/SoScalarWave/BoundaryConditions/DirichletAnalytic.hpp"
+#include "Evolution/Systems/SoScalarWave/BoundaryConditions/TimeDerivativeDirichlet.hpp"
 #include "Utilities/TMPL.hpp"
 
 namespace SoScalarWave::BoundaryConditions {
 /// Typelist of standard BoundaryConditions
 template <size_t Dim>
 using standard_boundary_conditions =
-    tmpl::list<DirichletAnalytic<Dim>,
+    tmpl::list<DirichletAnalytic<Dim>, TimeDerivativeDirichlet<Dim>,
                domain::BoundaryConditions::Periodic<BoundaryCondition<Dim>>>;
 }  // namespace SoScalarWave::BoundaryConditions
